04693da70e3ba5cdf4477115f969134cce143038
oxyethylene-macbook-pro <ljunyfor@outlook.com>
2023-04-27 00:36:27 +0800

    <feat>(global): <file table can work>

--- src/components/FileTable.vue
+++ src/components/FileTable.vue
@@ -3,16 +3,22 @@
     <el-table-column prop='name' label='Name' />
     <el-table-column label='Size' width='180' :formatter='sizeFormatter' prop='size' />
     <el-table-column label='Date' width='180' :formatter='dateFormatter' prop='mod_time' />
-    <el-table-column width='180'>
+    <el-table-column width='280'>
       <template #default='scope'>
+        <el-button
+          size='small'
+          type='primary'
+          @click='handleDownload(scope.$index, scope.row)'
+        >
+          Download
+        </el-button>
         <el-button
           size='small'
           type='danger'
           @click='handleDelete(scope.$index, scope.row)'
         >
           Delete
-        </el-button
-        >
+        </el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -20,6 +26,8 @@
 
 <script lang='ts' setup>
 import bytes from 'bytes'
+import axios from 'axios'
+import { onMounted, ref, Ref } from 'vue'
 
 interface File {
   name: string
@@ -27,26 +35,26 @@ interface File {
   mod_time: number
 }
 
-const tableData: File[] = [
-  {
-    'name': 'a.txt',
-    'size': 11,
-    'mod_time': 1679079579
-  },
-  {
-    'name': 'b.txt.go',
-    'size': 13,
-    'mod_time': 1679079579
-  },
-  {
-    'name': 'bili-evelod.json',
-    'size': 6826,
-    'mod_time': 1681142151
-  }
-]
+const tableData: Ref<File[]> = ref<File[]>([])
+
+const updateTableData = () => {
+  axios.get('http://127.0.0.1:8080/files')
+    .then(resp => {
+      console.log(resp)
+      tableData.value = resp.data.data
+    })
+    .catch((reason) => {
+      console.error(reason)
+      tableData.value = []
+    })
+}
+
+const handleDownload = (index: number, row: File) => {
+  console.log(`downloading file ${row.name} at index ${index}`)
+}
 
 const handleDelete = (index: number, row: File) => {
-  console.log(index, row)
+  console.log(`deleting file ${row.name} at index ${index}`)
 }
 
 const sizeFormatter = (row: number, column: number, cellValue: number, index: number) => {
@@ -56,6 +64,10 @@ const sizeFormatter = (row: number, column: number, cellValue: number, index: nu
 const dateFormatter = (row: number, column: number, cellValue: number, index: number) => {
   return new Date(cellValue * 1000).toLocaleString()
 }
+
+onMounted(() => {
+  updateTableData()
+})
 </script>
 
 <style scoped>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,9 +1,11 @@
 <template>
+  <UploadFile />
   <FileTable />
 </template>
 
 <script lang='ts' setup>
 import FileTable from '@/components/FileTable.vue'
+import UploadFile from '@/components/UploadFile.vue'
 </script>
 
 <style scoped>
